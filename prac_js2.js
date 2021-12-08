b60c4974f17958bdca0cb64d6c088224984fe1d7
aaliya-mondesir <75403684+aaliya-mondesir@users.noreply.github.com>
2021-12-08 01:54:12 -0500

    Add files via upload

--- prac_js2.js
+++ prac_js2.js
@@ -47,9 +47,52 @@ function another(arr){
             newArr.push(arr[i])
         }
     }
-    console.log()
-    return newArr;
+    console.log(arr[arr.length-2])
+    return newArr[0];
 }
 
 var array = [4,2,5,1,6,9];
 another(array);
+
+//4
+function double(arr){
+    var newArr = [];
+    for(var i=0; i<arr.length; i++){
+        newArr.push(arr[i]*2)
+    }
+    return newArr;
+}
+
+var array = [2,5,6];
+double(array);
+
+//5
+function positives(arr){
+    var counter = 0;
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]>0){
+            counter++;
+        }
+    }
+    arr[arr.length - 1] = counter;
+    return arr;
+}
+
+var array = [-1,2,-3,-5,-6,8,9];
+positives(array);
+
+//6
+function evenOdd(arr){
+    for(var i=0; i<arr.length; i++){
+        
+        if((arr[i]%2 == 1 )&& (arr[i+1]%2 == 1) && (arr[i+2] == 1)){
+            console.log("That's Odd!")
+        }
+    }
+    return arr;
+}
+
+var array = [3,5,7];
+evenOdd(array);
+
+//7
